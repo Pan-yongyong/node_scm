4e46727d39904261dfd2f65e2ace2dc43ceb60d2
PYY <panyy-c@glodon.com>
2022-03-18 16:22:41 +0800

    轮播图修改和删除

--- routes/admin/rotation.js
+++ routes/admin/rotation.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const url = require('url')
+const fs = require('fs')
 const Rotationdb = require('../../mdule/admin/rotation.js')
 const upload = require('../../util/multer.js')
-router.post('/add', upload.single('singleFile'), function(req, res) {
+router.post('/add', upload.single('img_url'), function(req, res) {
 	let img_url = req.file.path
 	let title = req.body.title
 	if(title && img_url) {
@@ -17,4 +19,34 @@ router.post('/add', upload.single('singleFile'), function(req, res) {
 	}
 })
 
+// 修改
+router.post('/edit',upload.single('img_url'),async function(req, res) {
+	let edId = req.body._id
+	let img_url = req.file.path
+	try{
+		await Rotationdb.updateOne({_id: edId}, {$set:Object.assign(req.body, {img_url})})
+		res.send('修改成功')
+	}catch(e){
+		res.status(404).send(e)
+	}
+})
+
+// 删除
+router.delete('/delete',async function(req, res) {
+	let _id = url.parse(req.url, true).query
+	try{
+		let reslut = await Rotationdb.find({_id})
+		await fs.unlink(reslut[0].img_url,async function(err, data) {
+			if(err) {
+				res.send(err)
+				return
+			}
+			await Rotationdb.deleteOne({_id})
+			res.send('删除成功')
+		})
+	}catch(e){
+		//TODO handle the exception
+		res.send(e)
+	}
+})
 module.exports = router
